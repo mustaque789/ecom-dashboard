04b08327531fcb8c1548164c4dfd922a65609b77
Mustaque Khan <130723242+mustaque789@users.noreply.github.com>
2024-01-17 07:47:37 -0500

    mongoose connection

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
-app.get('/', (req,res)=>{
-    res.send("App is working")
-})
+const connectDB = async ()=>{
+    mongoose.connect('mongodb://127.0.0.1:27017/e-comm');   // mongoose connection with the database 'e-comm'
+
+    const productSchema= new mongoose.Schema({});    // Schema :- we can keep schema empty if we are fetching data or while using GET method
+
+    const product = mongoose.model('product',productSchema);   // Model : Connection between mongodb and node js via schema 
+
+    const data = await product.find();   // finding data via GET method
+    console.warn(data);
+}
+
+connectDB();
 
 app.listen(5000)
